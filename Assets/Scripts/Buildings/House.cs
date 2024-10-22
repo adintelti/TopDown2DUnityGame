12e8a192b5dc984cff26b0f5ae1b6b7399c54bd4
Adriano Gomes Pimentel <adriano.pimentel@programmersinc.com>
2024-10-22 19:58:05 -0300

    Adicionando construção

---
+++ Assets/Scripts/Buildings/House.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class House : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer houseSprite;
+    [SerializeField] private Transform point;
+    [SerializeField] private Color startColor;
+    [SerializeField] private Color endColor;
+    [SerializeField] private float timeToBuild;
+
+    private bool detectingPlayer;
+    private Player player;
+    private PlayerAnim playerAnim;
+
+    private float elipsedTime;
+    private bool buildStarted;
+
+    void Start()
+    {
+        player = FindObjectOfType<Player>();
+        playerAnim = player.GetComponent<PlayerAnim>();
+    }
+
+    void Update()
+    {
+        if(detectingPlayer && Input.GetKeyDown(KeyCode.E))
+        {
+            buildStarted = true;
+            playerAnim.OnHammeringStarted();
+            houseSprite.color = startColor;
+            player.transform.position = point.position;
+            player.isPaused = true;
+        }
+
+        if(buildStarted)
+        {
+            elipsedTime += Time.deltaTime;
+
+            if(elipsedTime >= timeToBuild)
+            {
+                //casa terminada
+                playerAnim.OnHammeringEnded();
+                houseSprite.color = endColor;
+                player.isPaused = false;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            detectingPlayer = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            detectingPlayer = false;
+        }
+    }
+}
