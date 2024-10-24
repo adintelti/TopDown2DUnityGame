39a73b06de21d7a63609c960f2e7fa35a8cf2d2d
Adriano <adintelti@gmail.com>
2024-10-24 20:40:49 -0300

    Adding BGM and SFX sounds

---
+++ Assets/Sounds/AudioManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance;
+    [SerializeField] private AudioSource _audioSource;
+
+    void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(instance);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void PlayBGM(AudioClip audio)
+    {
+        _audioSource.clip = audio;
+        _audioSource.Play();
+    }
+}
