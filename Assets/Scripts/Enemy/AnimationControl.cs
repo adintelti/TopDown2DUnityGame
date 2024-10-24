838ba6c1937b98497ed50f91a9b6f7535d38c66d
Adriano Gomes Pimentel <adriano.pimentel@programmersinc.com>
2024-10-24 15:59:53 -0300

    completando codigo inimigo

--- Assets/Scripts/Enemy/AnimationControl.cs
+++ Assets/Scripts/Enemy/AnimationControl.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 
 public class AnimationControl : MonoBehaviour
 {
+    [SerializeField] private Transform attackPoint;
+    [SerializeField] private float radius;
+    public LayerMask playerLayer;
+
     private Animator anim;
+    private PlayerAnim playerAnim;
+    private Skeleton skeleton;
 
     void Start()
     {
         anim = GetComponent<Animator>();
+        playerAnim = FindObjectOfType<PlayerAnim>();
+        skeleton = GetComponentInParent<Skeleton>();
     }
 
     // Update is called once per frame
@@ -20,5 +28,42 @@ public class AnimationControl : MonoBehaviour
     public void PlayAnim(int value)
     {
         anim.SetInteger("transition", value);
+    }
+
+    public void Attack()
+    {
+        if(!skeleton.isDead)
+        {
+            Collider2D hit = Physics2D.OverlapCircle(attackPoint.position, radius, playerLayer);
+
+            if(hit != null)
+            {
+                Debug.Log("player atingido");
+                playerAnim.OnHit();
+            }
+        }
+    }
+
+    public void OnHit()
+    {
+        if(skeleton.currentHealth <= 0)
+        {
+            skeleton.isDead = true;
+            anim.SetTrigger("death");
+
+            Destroy(skeleton.gameObject, 1f);
+        }
+        else
+        {
+            anim.SetTrigger("hit");
+            skeleton.currentHealth--;
+
+            skeleton.healthBar.fillAmount = skeleton.currentHealth / skeleton.totalHealth;
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(attackPoint.position, radius);
     }
 }
--- Assets/Scripts/Enemy/Skeleton.cs
+++ Assets/Scripts/Enemy/Skeleton.cs
@@ -2,9 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UI;
 
 public class Skeleton : MonoBehaviour
 {
+    [Header("Stats")]
+    public float totalHealth;
+    public float currentHealth;
+    public Image healthBar;
+    public bool isDead;
+
+    [Header("Components")]
     [SerializeField] private NavMeshAgent agent;
     [SerializeField] private AnimationControl animControl;
 
@@ -12,6 +20,7 @@ public class Skeleton : MonoBehaviour
 
     void Start()
     {
+        currentHealth = totalHealth;
         player = FindObjectOfType<Player>();
         agent.updateRotation = false;
         agent.updateUpAxis = false;
@@ -19,27 +28,30 @@ public class Skeleton : MonoBehaviour
 
     void Update()
     {
-        agent.SetDestination(player.transform.position);
+        if(!isDead)
+        {
+            agent.SetDestination(player.transform.position);
 
-        if(Vector2.Distance(transform.position, player.transform.position) <= agent.stoppingDistance)
-        {
-            //chegou no limite de distancia
-            animControl.PlayAnim(2); //atacando
-        }
-        else
-        {
-            //skeleton segue o player
-            animControl.PlayAnim(1); //andando
-        }
+            if(Vector2.Distance(transform.position, player.transform.position) <= agent.stoppingDistance)
+            {
+                //chegou no limite de distancia
+                animControl.PlayAnim(2); //atacando
+            }
+            else
+            {
+                //skeleton segue o player
+                animControl.PlayAnim(1); //andando
+            }
 
-        float posX = player.transform.position.x - transform.position.x;
-        if(posX > 0)
-        {
-            transform.eulerAngles = new Vector2(0, 0);
-        }
-        else
-        {
-            transform.eulerAngles = new Vector2(0, 180);
+            float posX = player.transform.position.x - transform.position.x;
+            if(posX > 0)
+            {
+                transform.eulerAngles = new Vector2(0, 0);
+            }
+            else
+            {
+                transform.eulerAngles = new Vector2(0, 180);
+            }
         }
     }
 }
