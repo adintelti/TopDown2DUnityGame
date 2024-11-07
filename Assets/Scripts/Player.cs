ca5e00f8f3c146dc53ee5795aaa32dee355720c7
Adriano Gomes Pimentel <adriano.pimentel@programmersinc.com>
2024-11-06 22:13:31 -0300

    finalizado

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -78,11 +78,6 @@ public class Player : MonoBehaviour
             OnDig();
             OnWatering();
         }
-
-        if(Input.GetKeyDown(KeyCode.Space))
-        {
-            SceneManager.LoadScene("TestSecondScene");
-        }
     }
 
     private void FixedUpdate()
